a30eb62758dee5e9bc8aed0e3392e4bbfdb12ca8
Stella Mazzillo <stellagress5@gmail.com>
2023-06-16 17:13:23 -0300

    changes?

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -6,7 +6,6 @@ function MoviesList( {movies} ){
     const renderMovies = Object.keys(movies).map((movieID) => (
         <li key={movieID}>
             <Link to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
-
         </li>
     ))
 
